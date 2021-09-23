4b60d86d2ca4920e64ba7dda9be4c90b69b7fad8
safonov_dmitriy <sdssds341@gmail.com>
2021-09-23 15:22:15 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import { createBrowserHistory } from "history";
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 import App from "./App";
 import "./i18nextInit";
 import "./index.css";
-import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 export const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Suspense fallback="...">
-    <Provider store={store}>
-      <Router history={history}>
-        <App />
-      </Router>
-    </Provider>
-  </Suspense>,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById("root")
 );
 
